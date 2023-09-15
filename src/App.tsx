782ff4d010e5f85f1a7b4daab9742db2197ada62
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-15 10:46:51 +0800

    configured app to be exposed as microfrontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header-MF2">
         MicroFrontend 2
       </header>
     </div>
